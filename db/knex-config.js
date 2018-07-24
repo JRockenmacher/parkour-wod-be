6fba5160e67e326b376749402f70f2e4a0d8b85d
JRocken <jp.rockenmacher@gmail.com>
2018-07-24 07:19:43 -0600

    tring to upload again

--- db/knex-config.js
+++ db/knex-config.js
@@ -3,3 +3,9 @@ const environment = process.env.NODE_ENV || 'development'
 const pg = require('knex')(config[environment])
 
 module.exports = pg
+
+// const knexConfig = require('./knexfile')
+// const configEnv = knexConfig[environment]
+
+// const knex = require('knex')
+// const connection = knex(configEnv)
